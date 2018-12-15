55bf30a7e95e9ad42b0676a9db01487f3bc861a4
Hamish O'Neill <hone075@aucklanduni.ac.nz>
2018-12-15 22:05:43 +1300

    Initial commit with working script.

---
+++ edit_chapt.py
@@ -0,0 +1,131 @@
+import contextlib
+import os
+import subprocess
+import sys
+
+FFMPEG = "ffmpeg"
+EDITOR = os.getenv("EDITOR", "vim")
+MAX_LENGTH = 1000000
+AUTOMATIC_CHAPTER = "Intro"
+TIMEBASE = 1000
+
+
+def int_to_str(int_in):
+	secs = int_in % 60
+	minutes = int_in // 60
+	return f"{minutes}:{secs:02d}"
+
+
+def str_to_int(str_in):
+	parts = str_in.split(":")
+	return int(parts[0])*60 + int(parts[1])
+
+
+# Check for command line argument
+if len(sys.argv) != 2:
+	print("USAGE: python edit_chapt.py VIDEO")
+	exit()
+
+# Setup temporary file names
+input_name = sys.argv[1]
+input_name_only, input_extension = os.path.splitext(input_name)
+input_meta_name = input_name + "_meta_in.ini"
+our_display_name = input_name + "_meta_display"
+output_meta_name = input_name + "_meta_out.ini"
+output_name = input_name_only + "_tmp_out" + input_extension
+
+try:
+	chapters = []
+	new_chapters = []
+
+	# Extract current metadata
+	extract_meta_args = [FFMPEG, '-y', '-i', input_name, '-f', 'ffmetadata', input_meta_name]
+	subprocess.check_output(extract_meta_args, stderr=subprocess.DEVNULL)
+	with open(input_meta_name) as input_meta:
+		temp_chapter = None
+		for line in input_meta.readlines():
+			line = line.strip()
+			if line == "[CHAPTER]":
+				if temp_chapter is not None:
+					chapters.append(temp_chapter)
+				temp_chapter = {}
+			elif temp_chapter is None:
+				continue
+			elif line.startswith("TIMEBASE"):
+				temp_chapter["TIMEBASE"] = int(line.replace("TIMEBASE=1/", ""))
+			elif line.startswith("START"):
+				temp_chapter["START"] = int(line.replace("START=", ""))
+			elif line.startswith("END"):
+				temp_chapter["END"] = int(line.replace("END=", ""))
+			elif line.startswith("title"):
+				temp_chapter["title"] = line.replace("title=", "")
+
+	if temp_chapter is not None:
+		chapters.append(temp_chapter)
+
+	if AUTOMATIC_CHAPTER and len(chapters) == 0:
+		chapters.append({
+			"TIMEBASE": TIMEBASE,
+			"START": 0,
+			"END": MAX_LENGTH * TIMEBASE,
+			"title": "Intro"
+		})
+
+	# Allow the user to edit a more readable file
+	with open(our_display_name, "w+") as our_display:
+		# Generate the display file
+		for chapter in chapters:
+			start = int_to_str(chapter["START"]//chapter["TIMEBASE"])
+			end = int_to_str(chapter["END"]//chapter["TIMEBASE"])
+			our_display.write(f"{chapter['title']} {start}\n")
+		our_display.flush()
+
+		# Let the user edit with vim
+		subprocess.call([EDITOR, our_display_name])
+		our_display.seek(0)
+
+		# Parse back what the user has made
+		for line in our_display.readlines():
+			line = line.strip()
+			if line == "":
+				continue
+
+			name, start = line.split(" ")
+			new_chapters.append({
+				"START": str_to_int(start),
+				"title": name
+			})
+
+	# Write back new meta file
+	with open(output_meta_name, "w") as output_meta:
+		output_meta.write(";FFMETADATA1\n\n")
+		for index, chapter in enumerate(new_chapters):
+			if index + 1 == len(new_chapters):
+				end = MAX_LENGTH
+			else:
+				next_chapter = new_chapters[index + 1]
+				end = next_chapter["START"]
+
+			output_meta.write("[CHAPTER]\n")
+			output_meta.write(f"TIMEBASE=1/{TIMEBASE}\n")
+			output_meta.write(f"START={chapter['START'] * TIMEBASE}\n")
+			output_meta.write(f"END={end * TIMEBASE}\n")
+			output_meta.write(f"title={chapter['title']}\n")
+
+	# Create a new video file with the metadata
+	run_args = [FFMPEG, '-y', '-i', input_name, '-i', output_meta_name,
+				'-map_metadata', '1', '-map_chapters', '1', '-codec', 'copy', output_name]
+	subprocess.check_output(run_args, stderr=subprocess.DEVNULL)
+
+	# Rename the files
+	os.replace(input_name, input_name + ".old")
+	os.rename(output_name, input_name)
+	print("New chapter metadata written to " + input_name)
+
+finally:
+	# Remove temporary files
+	with contextlib.suppress(FileNotFoundError):
+		os.remove(input_meta_name)
+		os.remove(our_display_name)
+		os.remove(output_meta_name)
+		os.remove(output_name)
